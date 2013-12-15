a10205c0227a8ab8578ace01be35425bd0322ca0
Rudolf Tammekivi <rtammekivi@gmail.com>
2013-12-15 01:29:43 -0600

    gralloc: Allow legacy devices to use PMEM for ADSP (camera).
    Partially bring back PMEM support for legacy devices having proprietary camera
    libraries stuck in PMEM. This change allows these devices to continue using
    PMEM for the camera, and ION for everything else.
    
    Change-Id: I1382381134db21435ca725d0e2c2b3194da16f38

--- libgralloc/alloc_controller.cpp
+++ libgralloc/alloc_controller.cpp
@@ -34,10 +34,13 @@
 #include "memalloc.h"
 #ifdef USE_ION
 #include "ionalloc.h"
+#ifdef USE_PMEM_ADSP
+#include "pmemadspalloc.h"
+#endif
 #else
 #include "pmemalloc.h"
-#include "ashmemalloc.h"
 #endif
+#include "ashmemalloc.h"
 #include "gr.h"
 #include "comptype.h"
 
@@ -103,6 +106,9 @@ IAllocController* IAllocController::getInstance(bool useMasterHeap)
 IonController::IonController()
 {
     mIonAlloc = new IonAlloc();
+#ifdef USE_PMEM_ADSP
+    mPmemAlloc = new PmemAdspAlloc();
+#endif
 }
 
 int IonController::allocate(alloc_data& data, int usage)
@@ -113,6 +119,14 @@ int IonController::allocate(alloc_data& data, int usage)
 
     data.uncached = useUncached(usage);
     data.allocType = 0;
+
+#ifdef USE_PMEM_ADSP
+    if (usage & GRALLOC_USAGE_PRIVATE_ADSP_HEAP) {
+        data.allocType |= private_handle_t::PRIV_FLAGS_USES_PMEM_ADSP;
+        ret = mPmemAlloc->alloc_buffer(data);
+        return ret;
+    }
+#endif
 
     if(usage & GRALLOC_USAGE_PRIVATE_UI_CONTIG_HEAP)
         ionFlags |= ION_HEAP(ION_SF_HEAP_ID);
@@ -170,6 +184,10 @@ IMemAlloc* IonController::getAllocator(int flags)
     IMemAlloc* memalloc = NULL;
     if (flags & private_handle_t::PRIV_FLAGS_USES_ION) {
         memalloc = mIonAlloc;
+#ifdef USE_PMEM_ADSP
+    } else if (flags & private_handle_t::PRIV_FLAGS_USES_PMEM_ADSP) {
+        memalloc = mPmemAlloc;
+#endif
     } else {
         ALOGE("%s: Invalid flags passed: 0x%x", __FUNCTION__, flags);
     }
