f0ab99f6dd5e64d7ae99740b766189a178f33236
walter79 <gzwalter79@gmail.com>
2014-03-18 11:14:24 +0100

    add missing memtrack

--- Android.mk
+++ Android.mk
@@ -1,10 +1,12 @@
 ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),legacy)
-display-hals := libgralloc libgenlock libcopybit
-display-hals += libhwcomposer liboverlay libqdutils
-ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
-display-hals += liblight
-endif
+	display-hals := libgralloc libgenlock libcopybit
+	display-hals += libhwcomposer liboverlay libqdutils
+	display-hals += libmemtrack
+	
+	ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
+		display-hals += liblight
+	endif
 
-include $(call all-named-subdir-makefiles,$(display-hals))
+	include $(call all-named-subdir-makefiles,$(display-hals))
 
 endif
